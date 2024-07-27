b86191ec90115a6b2cb4ff7ba2fa86306f5222da
pero0125 <take_apple0125@icloud.com>
2024-07-27 13:06:33 +0900

    add cdkL3

---
+++ cdkL3/lib/cdk_l3-stack.ts
@@ -0,0 +1,89 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as ecs from 'aws-cdk-lib/aws-ecs';
+import * as ecr from 'aws-cdk-lib/aws-ecr';
+import * as ecs_patterns from 'aws-cdk-lib/aws-ecs-patterns';
+import * as logs from 'aws-cdk-lib/aws-logs';
+import * as iam from 'aws-cdk-lib/aws-iam';
+
+export class CdkL3Stack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+    
+    // ECRリポジトリの参照
+    const repository = ecr.Repository.fromRepositoryName(this, 'NextJsRepository', 'nextjs-app');
+
+    // TaskRoleの定義
+    const taskRole = new iam.Role(this, 'EcsTaskRole', {
+      assumedBy: new iam.ServicePrincipal('ecs-tasks.amazonaws.com'),
+    });
+    // ポリシーの追加
+    taskRole.addManagedPolicy(
+      iam.ManagedPolicy.fromAwsManagedPolicyName('AWSXRayDaemonWriteAccess')
+    );
+    taskRole.addManagedPolicy(
+      iam.ManagedPolicy.fromAwsManagedPolicyName('CloudWatchFullAccess')
+    );
+    taskRole.addToPolicy(
+      new iam.PolicyStatement({
+        effect: iam.Effect.ALLOW,
+        actions: ['s3:*'],
+        resources: ['*'],
+      })
+    );
+
+    // ApplicationLoadBalancedFargateServiceの作成
+    const loadBalancedFargateService = new ecs_patterns.ApplicationLoadBalancedFargateService(this, 'Service', {
+      memoryLimitMiB: 512,
+      cpu: 256,
+      desiredCount: 1,
+      taskImageOptions: {
+        image: ecs.ContainerImage.fromEcrRepository(repository, "latest"),
+        containerPort: 8080,
+        taskRole: taskRole,
+        logDriver: ecs.LogDrivers.awsLogs({
+          streamPrefix: 'nextjs',
+          logGroup: new logs.LogGroup(this, 'NextJsLogGroup', {
+            logGroupName: 'my-nextjs-app-logs',
+            retention: logs.RetentionDays.ONE_WEEK,
+            removalPolicy: cdk.RemovalPolicy.DESTROY
+          }),
+        }),
+      },
+      publicLoadBalancer: true,
+    });
+    // ADOTサイドカーコンテナの追加
+    loadBalancedFargateService.taskDefinition.addContainer('AdotSidecar', {
+      image: ecs.ContainerImage.fromEcrRepository(repository, "adot"),
+      logging: ecs.LogDrivers.awsLogs({ 
+        streamPrefix: 'adot',
+        logGroup: new logs.LogGroup(this, 'AdotLogGroup', {
+          logGroupName: 'my-nextjs-adot-logs',
+          retention: logs.RetentionDays.ONE_WEEK,
+          removalPolicy: cdk.RemovalPolicy.DESTROY
+        }),
+      }),
+      command: ['--config=/otel-config.yaml'],
+    }).addPortMappings({
+      containerPort: 4318, // HTTP port for OTLP
+    });
+    // ヘルスチェックの設定
+    loadBalancedFargateService.targetGroup.configureHealthCheck({
+      path: '/healthcheck',
+      port: '8080',
+      interval: cdk.Duration.seconds(10),
+      timeout: cdk.Duration.seconds(5),
+      healthyThresholdCount: 2,
+      unhealthyThresholdCount: 3,
+    });
+
+    // CloudFormationの出力にALBのDNS名を追加
+    new cdk.CfnOutput(this, 'LoadBalancerDNSRollDice', {
+      value: `http://${loadBalancedFargateService.loadBalancer.loadBalancerDnsName}/rolldice`,
+    });
+
+    new cdk.CfnOutput(this, 'LoadBalancerDNSS3', {
+      value: `http://${loadBalancedFargateService.loadBalancer.loadBalancerDnsName}/s3buckets`,
+    });
+  }
+}
---
+++ cdkL3/test/cdk_l3.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkL3 from '../lib/cdk_l3-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk_l3-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkL3.CdkL3Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
